770f1c1912acf0a8b0094ad0edb6f12e520bb2ef
ibnatysalsabila <salsajpr01@gmail.com>
2024-03-28 17:53:27 +0700

    mencari elemen yang lebih besar dari vipot

--- quicksortt/quicksortt.cpp
+++ quicksortt/quicksortt.cpp
@@ -52,3 +52,8 @@ void q_short(int low, int high)
 	pivot = arr[low]; //step 2
 	i = low + 1; // step 3
 	j = high; // step 4
+
+	while (i <= j) // step 10
+	{
+		// search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high)) // step 5
