1bc549906ebe0d138e4f12847114d9035652955e
ibnatysalsabila <salsajpr01@gmail.com>
2024-03-28 17:25:12 +0700

    memasukan void swap

--- quicksortt/quicksortt.cpp
+++ quicksortt/quicksortt.cpp
@@ -32,3 +32,11 @@ void input() {
 		cin >> arr[i];
 	}
 }
+
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	mov_count++;
+}
